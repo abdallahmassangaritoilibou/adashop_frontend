4c0532e57a8489101397b354f1c1829e462d159d
Ange230700 <kouakouangeericstephane@gmail.com>
2025-05-16 21:05:35 +0200

    style: 💄  apply new google font

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,16 @@
 // src\app\app.component.ts
 
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { PrimeNG } from 'primeng/config';
 import { ToastModule } from 'primeng/toast';
-import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    ToastModule, // for <p-toast>
-    ButtonModule, // for <p-button>
-  ],
+  imports: [RouterOutlet, ToastModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent implements OnInit {
-  constructor(private readonly primeng: PrimeNG) {}
-
-  ngOnInit() {
-    this.primeng.ripple.set(true);
-  }
-
+export class AppComponent {
   title = 'adashop-frontend';
-
-  toggleDarkMode() {
-    document.documentElement.classList.toggle('p-app-dark');
-  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
 // src\app\app.config.ts
 
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
@@ -10,12 +10,15 @@ import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
         preset: Aura,
+        options: {
+          darkModeSelector: 'system',
+          cssLayer: false,
+        },
       },
     }),
   ],
